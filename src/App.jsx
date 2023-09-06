8fb66718fd451cec584a86a19d2a7559c6d8bb4a
Your Name <youremail@example.com>
2023-09-06 16:26:09 +0300

    add Questions component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+import data from "./data";
 const App = () => {
+  const [questions, setQuestion] = useState(data);
   return <h2>Accordion Starter</h2>;
 };
 export default App;
